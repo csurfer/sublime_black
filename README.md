b8bc0a4cf66a5f719f048b38a57496ef44c42256
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2018-05-01 10:13:14 +0530

    SublimeText3 package for black formatting.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # sublime_black
-Sublime Text package to format python code using black formatter.
+
+[![Licence](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/csurfer/sublime_black/master/LICENSE)
+[![Thanks](https://img.shields.io/badge/Say%20Thanks-!-1EAEDB.svg)](https://saythanks.io/to/csurfer)
+
+SublimeText3 package to format python code using black formatter.
+
+![Demo](https://i.imgur.com/10aO24T.gif)
+
+## Features
+
+* **Black: Diff** to see the changes that would be done to the file.
+* **Black: Format** to format the file in place using [black](https://github.com/ambv/black).
+
+## Prerequisites
+
+* Install [black](https://github.com/ambv/black) and ensure you can use it in command line to format files.
+
+## Setup
+
+### Directly from the repository
+
+Clone the git repo into your `Packages` folder and restart SublimeText3
+
+### Package Control
+
+Submitted to Package Control on May 1st 2018. Should be available soon!
+
+## Notes
+
+* Currently supports only saved file formatting due to conflict between SublimeText3 default python version (3.3) and [black](https://github.com/ambv/black) supported version (3.6).
+* More to come as soon as SublimeText supports latest python version.
+
+## Contributing
+
+### Bug Reports and Feature Requests
+
+Please use [issue tracker](https://github.com/csurfer/rake-nltk/issues) for reporting bugs or feature requests.
+
+### Development
+
+Pull requests are most welcome.
+
+### Buy the developer a cup of coffee!
+
+If you found the utility helpful you can buy me a cup of coffee using
+
+[![Donate](https://www.paypalobjects.com/webstatic/en_US/i/btn/png/silver-pill-paypal-44px.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=3BSBW7D45C4YN&lc=US&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted)
---
+++ black-sublime.py
@@ -0,0 +1,72 @@
+"""
+Sublime Text package to format Python code using `black`
+https://github.com/ambv/black formatter.
+"""
+import sublime
+import sublime_plugin
+
+
+def load_user_settings():
+    """Method to procure user settings for black."""
+    return sublime.load_settings("black.sublime-settings")
+
+
+def execute(window, encoding, command, file_name):
+    """Command to execute in window."""
+
+    # TODO(vishwas): Once Sublime text starts supporting Python3.6+ extend
+    # the same to format view strings in addition to the files. This is
+    # currently not possible as any python module imported within plugin
+    # would run in Python3.3 which doesn't support `black`.
+    if not file_name:
+        sublime.message_dialog(
+            "Black formatter can currently be run only on saved files."
+        )
+        return
+
+    # Check to ensure command is run only on python files.
+    if not file_name.endswith(".py"):
+        sublime.message_dialog(
+            "Black formatter can be used to format `.py` files only."
+        )
+        return
+
+    cmd = []
+    # To ensure we don't have any issues with respect to click's
+    # http://click.pocoo.org/5/python3/#python-3-surrogate-handling
+    if encoding:
+        cmd.append(encoding)
+
+    cmd.append(command)
+    cmd.append(file_name)
+    window.run_command("exec", {"shell_cmd": " ".join(cmd)})
+
+
+class black_format(sublime_plugin.WindowCommand):
+    """Command to format files in place using `black`."""
+
+    def run(self):
+        self.settings = load_user_settings()
+        self.file_name = self.window.active_view().file_name()
+
+        execute(
+            self.window,
+            self.settings.get("encoding", None),
+            "black -l {0}".format(self.settings.get("line_length", 88)),
+            self.file_name,
+        )
+
+
+class black_diff(sublime_plugin.WindowCommand):
+    """Command to show what would be different if formatted using `black`."""
+
+    def run(self):
+        self.settings = load_user_settings()
+        self.file_name = self.window.active_view().file_name()
+
+        execute(
+            self.window,
+            self.settings.get("encoding", None),
+            "black --diff",
+            self.file_name,
+        )
